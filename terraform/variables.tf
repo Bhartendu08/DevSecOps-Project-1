1d277fbc028ba1c09c9ac61dd0ec30b97ef10f29
bhartendu11 <bhartendukr04@gmail.com>
2026-04-01 19:25:08 +0530

    commit 1

---
+++ terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+    description = ""
+    type = string
+    default = "ap-south-1"
+  
+}
+variable "environment"{
+    description = "value"
+    type = string
+    default = "devops"
+
+}
+
+variable "cluster_name"{
+    description = "value"
+    type = string
+    default = "value"
+
+}
+
+variable "vpc_cider"{
+    description = "value"
+    type = string
+    default = ""
+
+}
